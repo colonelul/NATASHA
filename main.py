79e170aca068d4fa7726b1da21f13b4849428938
colonelul <claudiu9112@gmail.com>
2021-09-15 12:26:13 +0300

    add another file

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 11:18:48 2021
+
+@author: NeluColoNelu
+"""
+
+import kivy
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MainUiApp(App):
+    pass
+
+if __name__== '__main__':
+    MainUiApp().run()
+
+
